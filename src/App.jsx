fa6db06e094fdb80b6194dfac1e895f5579a4e2d
Elvan Hidayat <elvanhd@gmail.com>
2023-02-21 20:58:26 +0700

    create posts-list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import Post from './components/Post';
+import PostsList from './components/PostsList';
 
 export default function App() {
   return (
     <main>
-      <Post
-        author='Aaron'
-        body='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore quam dolorem recusandae distinctio qui voluptatem expedita quo cumque, nisi nesciunt veniam illo aliquam quaerat odit maxime similique obcaecati inventore amet.'
-      />
-      <Post
-        author='Billy'
-        body='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repellat unde corrupti aut ipsa ut ipsum perspiciatis ea cumque voluptas sapiente neque dicta asperiores, mollitia atque sit in architecto corporis.'
-      />
-      <Post
-        author='Cindy'
-        body='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vel repudiandae officiis consequatur delectus consectetur praesentium autem ullam asperiores ipsum fuga dolor error aspernatur labore eveniet, veritatis atque quae at.'
-      />
+      <PostsList />
     </main>
   );
 }
---
+++ src/components/PostsList.jsx
@@ -0,0 +1,21 @@
+import Post from './Post';
+import classes from './PostsList.module.css';
+
+export default function PostsList() {
+  return (
+    <ul className={classes.posts}>
+      <Post
+        author='Aaron'
+        body='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore quam dolorem recusandae distinctio qui voluptatem expedita quo cumque, nisi nesciunt veniam illo aliquam quaerat odit maxime similique obcaecati inventore amet.'
+      />
+      <Post
+        author='Billy'
+        body='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repellat unde corrupti aut ipsa ut ipsum perspiciatis ea cumque voluptas sapiente neque dicta asperiores, mollitia atque sit in architecto corporis.'
+      />
+      <Post
+        author='Cindy'
+        body='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vel repudiandae officiis consequatur delectus consectetur praesentium autem ullam asperiores ipsum fuga dolor error aspernatur labore eveniet, veritatis atque quae at.'
+      />
+    </ul>
+  );
+}
