719aea0e3cc1c3e7bad59f29855bac51303c78ca
Elvan Hidayat <elvanhd@gmail.com>
2023-02-21 20:17:00 +0700

    output dynamic values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
+const names = ['Aaron', 'Billy'];
+
 export default function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Elvan</p>
+      <p>{chosenName}</p>
       <p>React.js is awesome!</p>
     </div>
   );
