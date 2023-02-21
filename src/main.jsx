a9cfa2a08a1b20ef6ef0c72758cbd59cc94345b2
Elvan Hidayat <elvanhd@gmail.com>
2023-02-22 02:04:22 +0700

    add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import NewPost from './components/NewPost';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/create-post',
+    element: <NewPost />,
+  },
+]);
 
 const rootEl = document.getElementById('root');
 
 if (rootEl) {
   ReactDOM.createRoot(rootEl).render(
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
   );
 }
