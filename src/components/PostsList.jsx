a3597393f5bdf561a5296c9cc2ff3ee018fe9279
Elvan Hidayat <elvanhd@gmail.com>
2023-02-22 00:31:30 +0700

    lift the state up

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post
           author='Aaron'
           body='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore quam dolorem recusandae distinctio qui voluptatem expedita quo cumque, nisi nesciunt veniam illo aliquam quaerat odit maxime similique obcaecati inventore amet.'
